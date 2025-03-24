d5ce9e046ec70b86a8543d177a2d9334d6ad1322
Reymond thomas <thomreymond38@gmail.com>
2025-03-24 12:34:23 +0100

    Update proxy, route universel

--- proxy/server.mjs
+++ proxy/server.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import fetch from 'node-fetch';
 const app = express();
 const port = 3000;
-const apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjViOGVkNjVjLTEyMmQtNDMzNy1hMTYyLTU0Y2Y3MTYwNDk4MyIsImlhdCI6MTc0Mjc0MjEyNSwic3ViIjoiZGV2ZWxvcGVyL2I4YTQ5MGE0LWQ3MmUtZDVkYy1lYWYyLTExOGVmNzNhOTVhMyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODIuNjQuMjQ0LjY2Il0sInR5cGUiOiJjbGllbnQifV19.NXzkII_NscCaPfqMKFLHWMdoO9fzdDtDsgYO4WUNRyTrsc6oiapb5pbLI8aHVEX6sIGWskUqMcl5zCc9Cq9ixg"
+const apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjUzYmFhYThhLTBmNzEtNDg1YS04YmI4LThjMDcyYWZhYjEzNiIsImlhdCI6MTc0MjgwNDM1NCwic3ViIjoiZGV2ZWxvcGVyL2I4YTQ5MGE0LWQ3MmUtZDVkYy1lYWYyLTExOGVmNzNhOTVhMyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTMwLjE5MC4xMTQuMTAzIl0sInR5cGUiOiJjbGllbnQifV19.c2IxSipll43ikndno731q4U8OGFkxVoFiRUz-bfDti0At-7z9W2UFDYMJGHMgE35T73n-skvYkh7HXi40uoYGw"
 app.use(express.json());
 
 // Middleware pour gérer les en-têtes CORS
@@ -13,9 +13,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/v1/brawlers', async (req, res) => {
-    const apiUrl = 'https://api.brawlstars.com/v1/brawlers';
-    
+app.get('/v1/:route', async (req, res) => {
+  const route = req.params.route;
+  const apiUrl = `https://api.brawlstars.com/v1/${route}`;
+  console.log(apiUrl);
     try {
         const response = await fetch(apiUrl, {
             method: 'GET',
@@ -30,7 +31,7 @@ app.get('/v1/brawlers', async (req, res) => {
         res.status(500).json({ error: 'Erreur lors de la requête à l\'API externe' });
     }
 });
-
+/*
 app.get('/v1/rankings/global/brawlers/:idChampion', async (req, res) => {
     const idChampion = req.params.idChampion;
     const apiUrl = `https://api.brawlstars.com/v1/rankings/global/brawlers/${idChampion}`;
@@ -68,6 +69,7 @@ app.get('/v1/events/rotation', async (req, res) => {
     res.status(500).json({ error: 'Erreur lors de la requête à l\'API externe' });
   }
 });
+*/
 app.listen(port, () => {
     console.log(`Serveur proxy en écoute sur le port ${port}`);
 });
